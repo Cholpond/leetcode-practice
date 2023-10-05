6e77d13d27860199376d94bbf15938365e6c6290
cholpond <dobulbekovach@gmail.com>
2023-10-04 22:24:04 -0700

    solved toBeNotToBe problem with my solution

--- easyQuestions.js
+++ easyQuestions.js
@@ -49,3 +49,35 @@ var argumentsLength = function(...args) {
                 }, millis)
             })
             }
+
+/** ______________________________________________
+ * 2704. To Be Or Not To Be
+Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+
+toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
+
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ * 
+ * created functions in an object to solve it with construction functions.
+ */
+var expect = function(val) {
+    return {
+        toBe: function(newVal){
+            if(val === newVal){
+                return true;
+            }else{
+                throw new Error ('Not Equal');
+            }
+        },
+        notToBe: function(newVal){
+            if(val !== newVal){
+                return true;
+            }else{
+                throw new Error ('Equal')
+            }
+        }
+    }
+};
+
