60bdba1c61151a7b4b2ecf656b59609bd96ca19c
cholpond <dobulbekovach@gmail.com>
2023-10-06 01:37:40 -0700

    added 2 solutions

--- easyQuestions.js
+++ easyQuestions.js
@@ -81,3 +81,59 @@ var expect = function(val) {
     }
 };
 
+/**____________________________________________
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ * Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
+
+The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
+
+The function composition of an empty list of functions is the identity function f(x) = x.
+
+You may assume each function in the array accepts one integer as input and returns one integer as output.
+
+ */
+var compose = function(functions) {
+    return function (input) {
+        return functions.reduceRight((acc, fn) => fn(acc), input);
+    };
+};
+
+/** _______________________________________________
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+
+Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+The first time the returned function is called, it should return the same result as fn.
+Every subsequent time it is called, it should return undefined.
+ */
+
+var once = function(fn) {
+    let executed = false;
+        let result;
+    
+        return function (...args) {
+            if (!executed) {
+                result = fn(...args);
+                executed = true;
+                return result;
+            } else {
+                return undefined;
+            }
+        };
+    };
+
+/**_____________________________________________________
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ * Given two promises promise1 and promise2, return a new promise. promise1 and promise2 will both resolve with a number. The returned promise should resolve with the sum of the two numbers.
+ */
+
+var addTwoPromises = async function(promise1, promise2) {
+    const [value1, value2] = await Promise.all([promise1, promise2]);
+    return value1 + value2;
+};
