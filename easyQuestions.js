1cff20b97a5e4827cea334627e55067ad1dab49f
cholpond <dobulbekovach@gmail.com>
2023-10-07 21:52:58 -0700

    added new problems solved from thirty day challenge

--- easyQuestions.js
+++ easyQuestions.js
@@ -137,3 +137,148 @@ var addTwoPromises = async function(promise1, promise2) {
     const [value1, value2] = await Promise.all([promise1, promise2]);
     return value1 + value2;
 };
+
+/**_____________________________________________________
+ 2635. Apply Transform Over Each Element in Array
+    Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+
+    The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+    Please solve it without the built-in Array.map method.
+ */
+    var map = function(arr, fn) {
+        let result = [];
+        //i created an empty array;
+       for(let i=0; i<arr.length; i++){
+           result.push(fn(arr[i], i))
+       }
+       //looped through the arr
+       //pushed it to the new arr with the function;
+       return result
+    };
+
+/**_____________________________________________________
+ 2626. Array Reduce Transformation
+
+Given an integer array nums, a reducer function fn, and an initial value init, return a reduced array.
+
+A reduced array is created by applying the following operation: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The final value of val is returned.
+
+If the length of the array is 0, it should return init.
+
+Please solve it without using the built-in Array.reduce method.
+TC: O(N);
+SC: O(1); - because the init and accumulate both always single values;
+
+ */    
+var reduce = function(nums, fn, init) {
+    //check if the nums is empty, if it is return 0; 
+    //create a new arr;
+    //loop through the nums; 
+    // and push everything into it;
+    //return new arr
+ if(nums.length === 0) return init; 
+ let result = init;
+ for(let i=0; i<nums.length; i++){
+    result = fn(result, nums[i]);
+ }
+ return result;
+ };
+
+/**_____________________________________________________
+ 2715. Timeout Cancellation
+    Given a function fn, an array of arguments args, and a timeout t in milliseconds, return a cancel function cancelFn.
+
+    After a delay of t, fn should be called with args passed as parameters unless cancelFn was invoked before the delay of t milliseconds elapses, specifically at cancelT ms. In that case, fn should never be called.
+
+    this is the logic when it can be used: 
+    Imagine you're building a notification system for a web application. When a new notification comes in, you want to display it to the user in a non-intrusive way, like a small popup in the corner of the screen. However, you also want to give the user the option to dismiss the notification if they choose.
+
+    Here's how you could use the cancellable function in this scenario:
+
+    When a new notification arrives, you use setTimeout to delay showing the notification for a short period (e.g., 5 seconds). This gives the user a moment to see the notification before it disappears.
+
+    You also provide a "dismiss" button on the notification. If the user clicks the "dismiss" button within the delay period (e.g., 5 seconds), you call the canceller function to cancel the timeout, preventing the notification from being displayed.
+
+    If the user doesn't dismiss the notification within the delay period, the setTimeout triggers the display of the notification.
+
+    By using a cancellable timeout, you allow the user to interact with the notification during the delay period, and if they choose to dismiss it, you can cancel the display before it occurs.
+
+    This approach provides a better user experience by allowing the user to control when they see and interact with notifications.
+    TC: O(1);
+    SC: O(1);
+ */    
+    var cancellable = function(fn, args, t) {
+        const timeOut = setTimeout(() => {
+            fn(...args)
+        }, t);
+    
+        return function(){
+            clearTimeout(timeOut);
+        }
+    };
+    
+/**_____________________________________________________
+    2725. Interval Cancellation
+    Given a function fn, an array of arguments args, and an interval time t, return a cancel function cancelFn.
+
+    The function fn should be called with args immediately and then called again every t milliseconds until cancelFn is called at cancelT ms.
+
+ */    
+    var cancellable = function(fn, args, t) {
+        fn(...args);  // Call the function immediately before setting up the interval;
+    
+        const timer = setInterval(() => {
+            fn(...args);
+        }, t);
+        // we call the function again every t (miliseconds) until the clearInterval is called. 
+
+        return function() { 
+            clearInterval(timer);
+        };
+        // Return a function to cancel the interval
+        // it will ensure that the provided function (fn) won't be executed repeatedly; 
+        //TC: O(1); - The overall time complexity is O(f) for invoking the provided function plus O(1) for setting and clearing the interval.
+        //SC: O(1); - Overall, the space complexity is O(1) because the amount of additional memory used is constant regardless of the input parameters.
+    };
+
+/**_____________________________________________________
+    2727. Is Object Empty
+    Given an object or an array, return if it is empty.
+
+    An empty object contains no key-value pairs.
+    An empty array contains no elements.
+    You may assume the object or array is the output of JSON.parse.
+ */ 
+
+    var isEmpty = function(obj) {
+        /** Array.isArray(obj): This is a method in JavaScript that determines whether the passed value (obj in this case) is an array. It returns true if obj is an array, and false if it is not.
+
+        return obj.length === 0;: If obj is determined to be an array (using Array.isArray(obj)), we then check if the length of the array is zero (obj.length === 0). If the length is zero, it means the array is empty, and we return true. Otherwise, we return false, indicating that the array is not empty.
+
+        In summary:
+
+        If the obj is an array (Array.isArray(obj) returns true), we check if it's empty by verifying if its length is zero (obj.length === 0).
+        If it's an empty array, we return true to indicate that it's empty.
+        If it's not an array or if it's a non-empty array, we return false to indicate that it's not empty. */
+        if(Array.isArray(obj)){
+            return obj.length === 0;
+        }
+        //first, we check if the obj is array, if it is an empty array, we return true; 
+        if(obj && typeof obj === 'object'){
+            //check if obj is an object;
+            //if so, we loop through each property
+            //if obj has any property, we return false;
+            for(let key in obj){
+                if(obj.hasOwnProperty(key)){
+                    return false;
+                }
+            }
+            //else, we return true;
+            return true;
+        }
+        //if the obj is not an array or an object, so it's non-empty, we return false; 
+        return false;
+        //TC: O(N);
+        //SC: O(1);
+    };
