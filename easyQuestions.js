a76454f0beda603cb2099db3c1b922d2e8d7b72d
cholpond <dobulbekovach@gmail.com>
2023-10-12 10:06:29 -0700

    solved one medium question and added the solution of it

--- easyQuestions.js
+++ easyQuestions.js
@@ -382,3 +382,84 @@ var reduce = function(nums, fn, init) {
         return `[${String(this.nums)}]`;
     }
 
+/**_____________________________________
+ * 2726. Calculator with Method Chaining
+    Design a Calculator class. The class should provide the mathematical operations of addition, subtraction, multiplication, division, and exponentiation. It should also allow consecutive operations to be performed using method chaining. The Calculator class constructor should accept a number which serves as the initial value of result.
+
+    Your Calculator class should have the following methods:
+
+    add - This method adds the given number value to the result and returns the updated Calculator.
+    subtract - This method subtracts the given number value from the result and returns the updated Calculator.
+    multiply - This method multiplies the result  by the given number value and returns the updated Calculator.
+    divide - This method divides the result by the given number value and returns the updated Calculator. If the passed value is 0, an error "Division by zero is not allowed" should be thrown.
+    power - This method raises the result to the power of the given number value and returns the updated Calculator.
+    getResult - This method returns the result.
+    Solutions within 10-5 of the actual result are considered correct.
+     
+    The time and space complexity for each method in the Calculator class is O(1), indicating constant time and constant space requirements. This is because the calculations and operations are not dependent on the size of any input; they involve a fixed number of operations and variables regardless of the input values.
+ */
+
+    class Calculator {
+        /**
+         * @param {number} value
+         */
+        constructor(value) {
+            this.result = value;
+        }
+    
+        /**
+         * @param {number} value
+         * @return {number}
+         */
+        add(value) {
+            this.result += value;
+            return this; 
+        }
+    
+        /**
+         * @param {number} value
+         * @return {number}
+         */
+        subtract(value) {
+            this.result -= value;
+            return this;
+        }
+    
+        /**
+         * @param {number} value
+         * @return {number}
+         */
+        multiply(value) {
+            this.result *= value;
+            return this;
+        }
+    
+        /**
+         * @param {number} value
+         * @return {number}
+         */
+        divide(value) {
+            if (value === 0) {
+                throw new Error("Division by zero is not allowed");
+            }
+            this.result /= value;
+            return this;
+        }
+    
+    
+        /**
+         * @param {number} value
+         * @return {number}
+         */
+        power(value) {
+            this.result = Math.pow(this.result, value);
+            return this; 
+        }
+    
+        /**
+         * @return {number}
+         */
+        getResult() {
+            return this.result;
+        }
+    }
